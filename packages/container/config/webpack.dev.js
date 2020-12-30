e72786960a69dee61282e8c3c19222cc582f41a5
sodurinde <odurinde.olusola@yahoo.com>
2020-12-30 22:20:22 +0100

    Created contaniner deploy scripts

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const {merge} = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 
@@ -22,9 +22,7 @@ const devConfig = {
             shared: ['react', 'react-dom']
 
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+       
     ]
 }
 
